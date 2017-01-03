e28ff2f57524736f4ff4c63179b49ec1c42622e7
dzollo <dzollo@swift-nav.com>
2017-01-03 15:24:38 -0800

    use dialout group instead

--- tasks/configure_udev_rules.sh
+++ tasks/configure_udev_rules.sh
@@ -1,5 +1,9 @@
+current_user="$USER"
+echo "Creating new udev rule 99-piksi.rules with the following lines"
 sudo tee /etc/udev/rules.d/99-piksi.rules <<EOF
-ATTRS{idProduct}=="6014", ATTRS{idVendor}=="0403", MODE="666", GROUP="plugdev"
-ATTRS{idProduct}=="8398", ATTRS{idVendor}=="0403", MODE="666", GROUP="plugdev"
-ATTRS{idProduct}=="A4A7", ATTRS{idVendor}=="0525", MODE="666", GROUP="plugdev"
+ATTRS{idProduct}=="6014", ATTRS{idVendor}=="0403", MODE="666", GROUP="dialout"
+ATTRS{idProduct}=="8398", ATTRS{idVendor}=="0403", MODE="666", GROUP="dialout"
+ATTRS{idProduct}=="A4A7", ATTRS{idVendor}=="0525", MODE="666", GROUP="dialout"
+echo "Adding current user to the dialout group."
+sudo usermod -a -G dialout $current_user 
 EOF
