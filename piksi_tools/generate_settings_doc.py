e034ea9ace5be71549961a26016a3338b9b6a162
dzollo <dzollo@swift-nav.com>
2018-10-16 13:59:23 -0700

    v2.1.14

--- piksi_tools/generate_settings_doc.py
+++ piksi_tools/generate_settings_doc.py
@@ -89,7 +89,7 @@ def main():
 
   latex_template = jenv.get_template('settings_template.tex')
   with open("settings_out.tex", 'w') as f:
-    f.write(latex_template.render(groups=sorted(groups), setting=sorted(settings.list_of_dicts), version='v2.1.11'))
+    f.write(latex_template.render(groups=sorted(groups), setting=sorted(settings.list_of_dicts), version='v2.1.14'))
 
   import subprocess
 
